f651c6a982c310241a284e0be898264bab33572d
Ajitesh Chhatani <ajiteshchhatani@gmail.com>
2023-06-01 19:37:08 +0400

    Adds modal dialog to show success message on successful form submission

--- validation_script.js
+++ validation_script.js
@@ -2,6 +2,9 @@ const form = document.getElementsByTagName("form");
 const email = document.getElementById("email");
 const errorLabel = document.getElementById("errorText");
 const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const dialog = document.getElementById("success-dialog");
+const dismissBtn = document.getElementById("dismiss-btn");
+const subscriber_email = document.getElementById("subscriber-email");
 
 let isEmailValid = false;
 
@@ -18,5 +21,17 @@ form[0].addEventListener("submit", (event) => {
     else {
         email.className = "valid";
         errorLabel.textContent = ""
+        dialog.style.display = "block";
+        subscriber_email.innerHTML = email.value;
+    }
+})
+
+dismissBtn.addEventListener("click", () => {
+    dialog.style.display = "none";
+})
+
+window.addEventListener("click", (event) => {
+    if(event.target === dialog) {
+        dialog.style.display = "none";
     }
 })
